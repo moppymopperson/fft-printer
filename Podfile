75f4f27c6ccb86a545e89283cba334fa33b8f112
Erik Hornberger <hornbergererik@gmail.com>
2018-06-21 22:03:22 +0900

    Initial commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'fft-printer' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for fft-printer
+  pod 'SwiftLint'
+  pod 'Socket.IO-Client-Swift'
+end
---
+++ fft-printer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fft-printer
+//
+//  Created by Erik Hornberger on 2018/06/16.
+//  Copyright © 2018 Erik Hornberger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
